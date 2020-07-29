93aede38b6f377396ace8d62ff241376edd332cc
gbruno360 <49034413+gbruno360@users.noreply.github.com>
2020-07-29 20:45:15 -0300

    Update script-POP-UP-NOVO.js

--- script-POP-UP-NOVO.js
+++ script-POP-UP-NOVO.js
@@ -1,15 +1,16 @@
+        function abrepopup() {
+        xvarONM = document.getElementById("popupONM");
+        xvarONM.style.display = "block";
+        }
+
+        function fechapopup() {
+        var xpopupONM = document.getElementById("popupONM");
+        if (xpopupONM.style.display === "none") {
+            xpopupONM.style.display = "block";
+        } else {
+            xpopupONM.style.display = "none";
+        }
+        }
+        console.log = 'funcionou';
+
 setTimeout(abrepopup(), 3000);
-
-function abrepopup() {
-    xvarONM = document.getElementById("popupONM");
-    xvarONM.style.display = "block";
-}
-
-function fechapopup() {
-    var xpopupONM = document.getElementById("popupONM");
-    if (xpopupONM.style.display === "none") {
-        xpopupONM.style.display = "block";
-    } else {
-        xpopupONM.style.display = "none";
-    }
-  }
