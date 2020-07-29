c977b699148a2911233beb43b2ba80262b185871
gbruno360 <49034413+gbruno360@users.noreply.github.com>
2020-07-29 09:20:39 -0300

    Add files via upload

---
+++ script-POP-UP-NOVO.js
@@ -0,0 +1,20 @@
+setTimeout(abrepopup(), 5000);
+
+function abrepopup() {
+    xvarONM = document.getElementById("popupONM");
+    xvarONM.style.display = "block";
+}
+
+function fechapopup() {
+    var xpopupONM = document.getElementById("popupONM");
+    if (xpopupONM.style.display === "none") {
+        xpopupONM.style.display = "block";
+    } else {
+        xpopupONM.style.display = "none";
+    }
+  }
+
+
+
+
+  onclick="fechapopup()"
